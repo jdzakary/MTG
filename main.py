20da7b1c8d58eea32dedd4b7fbfec4fc28d8e774
Davis <75322686+jdzakary@users.noreply.github.com>
2022-07-03 10:47:22 +0200

    Splash Screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from main_window import MainWindow
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QSplashScreen
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import sys
 import ctypes
 
@@ -12,6 +14,14 @@ screen_size = screen_list[0].size()
 scr_w = int(screen_size.width())
 scr_h = int(screen_size.height())
 
+splash_pix = QPixmap('Icons/Nova_Splash.png')
+splash_pix = splash_pix.scaledToHeight(int(461 * scr_w / 1980))
+splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+splash.setMask(splash_pix.mask())
+splash.show()
+app.processEvents()
+
 window = MainWindow(scr_w, scr_h)
 window.set_style('Styles/style.css')
+splash.finish(window)
 sys.exit(app.exec())
